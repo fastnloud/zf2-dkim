26596272d664d7d657363e78cf27c6bf70ae4a3a
jaap <thinkfast22>
2014-05-29 13:26:17 +0200

    Added Dkim header class.

---
+++ src/Dkim/Header/Dkim.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Dkim\Header;
+
+use Zend\Mail\Header\HeaderInterface;
+use Zend\Mail\Header\Exception;
+
+class Dkim implements HeaderInterface
+{
+
+    /**
+     * @var string
+     */
+    protected $value;
+
+    /**
+     * @param string $headerLine
+     * @return HeaderInterface|static
+     * @throws \Zend\Mail\Header\Exception\InvalidArgumentException
+     */
+    public static function fromString($headerLine)
+    {
+        list($name, $value) = GenericHeader::splitHeaderLine($headerLine);
+
+        // check to ensure proper header type for this factory
+        if (strtolower($name) !== 'dkimsignature') {
+            throw new Exception\InvalidArgumentException('Invalid header line for DKIM-Signature string');
+        }
+
+        $header = new static($value);
+
+        return $header;
+    }
+
+    /**
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFieldName()
+    {
+        return 'DKIM-Signature';
+    }
+
+    /**
+     * @param bool $format
+     * @return string
+     */
+    public function getFieldValue($format = HeaderInterface::FORMAT_RAW)
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $encoding
+     * @return $this|HeaderInterface
+     */
+    public function setEncoding($encoding)
+    {
+        // This header must be always in US-ASCII
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEncoding()
+    {
+        return 'ASCII';
+    }
+
+    /**
+     * @return string
+     */
+    public function toString()
+    {
+        return 'DKIM-Signature: ' . $this->getFieldValue();
+    }
+
+}
