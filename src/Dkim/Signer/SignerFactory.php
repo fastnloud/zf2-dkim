c26765232616f0a9dfddf362fe87d5b62a7d7c07
joepsyko <joepsyko@users.noreply.github.com>
2016-10-25 18:36:40 +0700

    Fix compatibility with zf2 and zf3

--- src/Dkim/Signer/SignerFactory.php
+++ src/Dkim/Signer/SignerFactory.php
@@ -2,11 +2,18 @@
 
 namespace Dkim\Signer;
 
-use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class SignerFactory implements FactoryInterface
 {
 
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $signer = new Signer($serviceLocator->get('Config'));
+        return $signer;
+    }
+    
     public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = null)
     {
         $signer = new Signer($container->get('Config'));
